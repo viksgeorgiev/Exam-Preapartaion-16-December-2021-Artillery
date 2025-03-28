c3741949f1d2c25c46a6370790bee4810dab95c2
Viks Georgiev <viks47@gmail.com>
2025-03-28 07:42:22 +0200

    Add project files.

---
+++ Artillery/Data/ArtilleryContext.cs
@@ -0,0 +1,31 @@
+﻿namespace Artillery.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+
+    public class ArtilleryContext : DbContext
+    {
+        public ArtilleryContext() 
+        { 
+        }
+
+        public ArtilleryContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
